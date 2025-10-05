35dbbb1bc20e02463bbf728402fd3a1947f6b2c2
Am66097 <https://github.com/Am66097>
2025-10-05 17:48:28 +0300

    Add project files.

---
+++ GymManagmentDAL/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Entities
+{
+    internal class BaseEntity
+    {
+
+
+    }
+}
