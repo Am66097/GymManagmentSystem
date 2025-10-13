2eaf187489586ed7532f5ceaad14c74793cb0b93
Am66097 <https://github.com/Am66097>
2025-10-13 14:39:53 +0300

    Session 04 : Home Page With Tag Helpers

--- GymManagmentPL/Controllers/HomeController.cs
+++ GymManagmentPL/Controllers/HomeController.cs
@@ -4,40 +4,42 @@ namespace GymManagmentPL.Controllers
 {
     public class HomeController : Controller
     {
+
+        #region Action Return Types
         public IActionResult Index() //IActionResult for all types of views
         {
             return View();
         }
 
-        public ViewResult About() //ViewResult for only views
-        {
-            ViewData["Message"] = "Your application description page.";
-            return View();
-        }
+        //public ViewResult About() //ViewResult for only views
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+        //    return View();
+        //}
 
-        public FileResult GetImage() //FileResult for files
-        {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
-            var fileBytes = System.IO.File.ReadAllBytes(filePath);
-            return File(fileBytes, "site/css");
-        }
+        //public FileResult GetImage() //FileResult for files
+        //{
+        //    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
+        //    var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        //    return File(fileBytes, "site/css");
+        //}
 
-        public JsonResult GetJson() //JsonResult for JSON data
-        {
-            var data = new { Name = "Gym Management System", Version = "1.0.0" };
-            return Json(data);
-        }
+        //public JsonResult GetJson() //JsonResult for JSON data
+        //{
+        //    var data = new { Name = "Gym Management System", Version = "1.0.0" };
+        //    return Json(data);
+        //}
 
-        public RedirectResult GoToGoogle() //RedirectResult for redirection
-        {
-            return Redirect("https://www.google.com");
-        }
-        public ContentResult GetContent() //ContentResult for plain text
-        {
-            return Content("<h1>Hello, this is a plain text response.</h1>","HTML");
-        }
+        //public RedirectResult GoToGoogle() //RedirectResult for redirection
+        //{
+        //    return Redirect("https://www.google.com");
+        //}
+        //public ContentResult GetContent() //ContentResult for plain text
+        //{
+        //    return Content("<h1>Hello, this is a plain text response.</h1>","HTML");
+        //} 
 
-
+        #endregion
 
 
 
